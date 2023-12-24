84afe2c4e75abe14f08ecca6c889dd1815881668
bdshaff <bendenisshaffer@gmail.com>
2023-12-24 12:09:50 -0500

    geometric adstock weights docu

--- R/geometric_adstock_weights.R
+++ R/geometric_adstock_weights.R
@@ -1,8 +1,20 @@
-#'Geometric Adstock Weights
+#' Geometric Adstock Weights
 #'
 #' @param decay decay rate
 #' @param max_carryover maximum carryover
 #' @param normalize  should the result be scaled?
+#' @returns a numeric vector of weights of length (max_carryover)
+#'
+#' @description
+#' geometric_adstock_weights is a function that is called to generate a vector of weights
+#' that define how media impressions will be decayed in present and carried over into the future.
+#'
+#'
+#' @examples
+#' geometric_adstock_weights()
+#' geometric_adstock_weights(decay = 0.1)
+#' geometric_adstock_weights(max_carryover = 3)
+#' geometric_adstock_weights(decay = 0.4, max_carryover = 4, normalize = FALSE)
 #'
 #'@export
 
